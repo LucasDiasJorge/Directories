bb34143c5a2d97b27ace3bf0abfa398ee49e38e1
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-01-24 17:13:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <string.h>
+#include <signal.h>
+
+volatile sig_atomic_t keep_running = 1;
+
+void sigint_handler(int signum) {
+    // Manipulador de sinal para SIGINT (Ctrl+C)
+    printf("\nRecebido sinal SIGINT (Ctrl+C). Saindo...\n");
+    keep_running = 0;
+}
 
 void listFiles(const char *path) {
     DIR *dir;
@@ -25,10 +35,26 @@ void listFiles(const char *path) {
 }
 
 int main() {
-    const char *path = "."; // Altere "." para o caminho do diretório desejado
+    signal(SIGINT, sigint_handler);  // Configura o handler para SIGINT (Ctrl+C)
 
-    printf("Listando arquivos em %s:\n", path);
-    listFiles(path);
+    while (keep_running) {
+        char path[100];  // Supõe que o caminho do diretório não excederá 100 caracteres
+
+        // Solicita ao usuário o caminho do diretório desejado
+        printf("Digite o caminho do diretório desejado (ou !exit para sair): ");
+        scanf("%s", path);
+
+        if (strcmp(path, "!exit") == 0) {
+            keep_running = 0;
+            break;
+        }
+
+        // Chama a função para listar os arquivos no diretório fornecido pelo usuário
+        printf("Listando arquivos em %s:\n", path);
+        listFiles(path);
+    }
+
+    printf("Encerrando o programa.\n");
 
     return 0;
 }
